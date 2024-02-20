b8ea5f9d7dedcec81e2e1dc14664a5c4f684c14d
EquipoUce <equipouce5@gmail.com>
2024-02-20 15:39:21 -0500

    AppSemiFinal4

--- app/src/main/java/com/example/ucemap/ui/MapaActivity.java
+++ app/src/main/java/com/example/ucemap/ui/MapaActivity.java
@@ -2,22 +2,15 @@ package com.example.ucemap.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.Manifest;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.ucemap.R;
 import com.example.ucemap.repository.modelo.Posicion;
@@ -32,14 +25,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -51,25 +41,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 public class MapaActivity extends AppCompatActivity implements OnMapReadyCallback {
-
     private Button botonGenerarCamino;
     private Button botonDetalles;
     private Button botonInformacion;
+    private Button botonCamino2;
     private Intent intent;
     private InformacionGeneral informacion;
     private Posicion posicion;
-    private List<Posicion> entradasPosicion;
-
     //Variables para implementar el mapa.
     GoogleMap mapa;
-    private Marker destino, origen;
-
+    private Marker destino, destino1, origen, origen1;
     private LatLng destinoLL, origenLL;
-
     private List<LatLng> ruta;
+    private List<LatLng> rutaTeatro;
     private String nombreRuta;
-
-    public static final String DIRECTORIO_JASSON= "rutas.json";
+    public static final String DIRECTORIO_JASSON = "rutas.json";
 
 
     @Override
@@ -81,13 +67,10 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         botonGenerarCamino = findViewById(R.id.bottonCamino);
         botonDetalles = findViewById(R.id.bottonDetalles);
         botonInformacion = findViewById(R.id.bottonInformacion);
+        botonCamino2 = findViewById(R.id.bottonCamino2);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.idMapaGeneral);
         mapFragment.getMapAsync(this);
-
-        //Aplicamos el objeto informacionHolder para extraer la informacion que no cambia
-        InformacionHolder informacionHolder = InformacionHolder.obtenerInstancia();
-        EntradasHolder entradasHolder = EntradasHolder.obtenerInstancia();
 
         //Actualizar titulo de Layout dependiendo de la entidad escogida
         TextView textTiruloMapa = findViewById(R.id.textTituloMapa);
@@ -108,22 +91,73 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
 
-
         this.nombreRuta = InformacionHolder.getNombreEntidadAsociada();
-        try {
-            ruta = this.coordenadaRuta(this.leerJson(this));
-        } catch (Exception e) {
-            throw new RuntimeException(e.toString());
-        }
+
+
         //-------------------------------------------------------------------------------------------
         posicion = InformacionHolder.getInformacion().getPosicion(); //<--- Posicion
         //-------------------------------------------------------------------------------------------
         botonGenerarCamino.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MapaActivity.this, " "+InformacionHolder.getNombreEntidadAsociada(), Toast.LENGTH_SHORT).show();
+                mapa.clear();
+                try {
+                    ruta = coordenadaRuta(leerJson(MapaActivity.this, DIRECTORIO_JASSON), "coordenadas");
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
                 drawRoute(ruta);
-                //finish();
+
+                origenLL = ruta.get(0);
+                destinoLL = ruta.get(ruta.size() - 1);
+
+                origen1 = mapa.addMarker(new MarkerOptions()
+                        .title("ESTA AQUI")
+                        .position(origenLL)
+                );
+
+
+                destino1 = mapa.addMarker(new MarkerOptions()
+                        .title(nombreRuta)
+                        .position(destinoLL)
+                );
+                destino1.showInfoWindow();
+
+                mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(destinoLL, 15));
+            }
+        });
+
+        botonCamino2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapa.clear();
+                try {
+                    rutaTeatro = coordenadaRuta(leerJson(MapaActivity.this, DIRECTORIO_JASSON), "coordenadas2");
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                drawRoute(rutaTeatro);
+
+                origenLL = rutaTeatro.get(0);
+                destinoLL = rutaTeatro.get(rutaTeatro.size() - 1);
+
+                origen = mapa.addMarker(new MarkerOptions()
+                        .title("ESTA AQUI")
+                        .position(origenLL)
+                );
+
+
+                destino = mapa.addMarker(new MarkerOptions()
+                        .title(nombreRuta + " - Teatro")
+                        .position(destinoLL)
+                );
+               destino.showInfoWindow();
+
+                mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(destinoLL,15));
+
+
             }
         });
 
@@ -132,7 +166,7 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (!EntradasHolder.getInformacionInicializada()) {
             try {
 
-                IEntradaService iEntradaService =  new EntradaServiceImpl();
+                IEntradaService iEntradaService = new EntradaServiceImpl();
                 List<Posicion> tmp = iEntradaService.buscarTodasLasEntradasPorIdentificador(this);
                 EntradasHolder.setEntradasPosiciones(tmp);
                 EntradasHolder.setInformacionInicializada(true);
@@ -145,7 +179,6 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         //Me tocan mi parte del codigo y los saco del grupo
         //-------------------------------------------------------------------------------------------
-        entradasPosicion = EntradasHolder.getEntradasPosiciones();
         //-------------------------------------------------------------------------------------------
 
         //-------------------------------------------------------------------------------------------
@@ -159,7 +192,6 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
                 //finish();
             }
         });
-
 
 
         botonInformacion.setOnClickListener(new View.OnClickListener() {
@@ -179,41 +211,30 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         startActivity(intent);
         finish();
     }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mapa = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-        origenLL = ruta.get(0);
-        destinoLL = ruta.get(ruta.size()-1);
-
-
-        origen = mapa.addMarker(new MarkerOptions()
-                .title("ESTA AQUI")
-                .position(origenLL)
+        LatLng inicio = new LatLng(posicion.getLatitud(),posicion.getLongitud());
+        destino1 = mapa.addMarker(new MarkerOptions()
+                .title(posicion.getIdentificador())
+                .position(inicio)
         );
-        origen.showInfoWindow();
+       destino1.showInfoWindow();
 
-        destino = mapa.addMarker(new MarkerOptions()
-                .title(this.nombreRuta)
-                .position(destinoLL)
-        );
-        //destino.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.baseline_navigate_next_black_24dp));
-        destino.showInfoWindow();
-
-
-        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(destinoLL, (float) 17.5F));
-
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(inicio, (float) 17.5F));
     }
     @NonNull
-    private List<LatLng> coordenadaRuta(String json) throws Exception {
+    private List<LatLng> coordenadaRuta(String json,String nombre) throws Exception {
         List<LatLng> coordenadas = new ArrayList<>();
 
         JSONObject jsonO = new JSONObject(json);
         JSONArray features = jsonO.getJSONArray(this.nombreRuta);
         if (features.length() > 0) {
             JSONArray vectorCoordenadas = features.getJSONObject(0)
-                    .getJSONArray("coordenadas");
+                    .getJSONObject("camino")
+                    .getJSONArray(nombre);
 
             for (int i = 0; i < vectorCoordenadas.length(); i++) {
                 JSONArray coordenada = vectorCoordenadas.getJSONArray(i);
@@ -225,11 +246,12 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         return coordenadas;
     }
+
     //RUTAS
-    private String leerJson(Context context) throws IOException
+    private String leerJson(Context context, String nombre) throws IOException
     {
         BufferedReader reader = null;
-        reader = new BufferedReader(new InputStreamReader(context.getAssets().open(DIRECTORIO_JASSON), "UTF-8"));
+        reader = new BufferedReader(new InputStreamReader(context.getAssets().open(nombre), "UTF-8"));
         String content = "";
         String line;
         while ((line = reader.readLine()) != null)
@@ -238,14 +260,11 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         return content;
     }
-
     private void drawRoute(List<LatLng> routeCoordinates) {
         PolylineOptions polylineOptions = new PolylineOptions()
                 .addAll(routeCoordinates)
                 .width(10)
                 .color(Color.BLUE);
-
         Polyline polyline = mapa.addPolyline(polylineOptions);
-        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(routeCoordinates.get(0), 15));
     }
 }
